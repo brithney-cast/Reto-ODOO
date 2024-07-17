0dfb865c104ce1ea42748262f49b9a62afbb753e
Brithney Castillon <121842372+brithney-cast@users.noreply.github.com>
2024-07-17 20:37:05 +0000

    [IMP] div_inventory: Añadido método create al modelo order Se creó el método create al modelo order porque se requiere validar que los campos se hayan ingresado antes de guardar el registro en la base de datos

--- extra-addons/dv_inventory/models/order.py
+++ extra-addons/dv_inventory/models/order.py
@@ -1,10 +1,26 @@
-from odoo import models, fields
+from odoo import models, fields, api, exceptions
 
 class Order (models.Model):
     _name = "inventory.order"
     _description = "Orden de venta"
 
-    name = fields.Char(string="Nombre de  orden de venta", required=True)
+    name = fields.Char(string="Nombre de  orden de venta")
     date_order = fields.Datetime(string="Fecha de orden de venta", default=fields.Datetime.now)
+    amount_total = fields.Float(string="Monto total")
+
     client_id = fields.Many2one("inventory.client", string="Cliente", required=True)
-    order_line_ids = fields.One2many("inventory.order_line", "order_id", string="Línea de órdenes de venta", required=True)
+    order_line_ids = fields.One2many("inventory.order_line", "order_id", string="Línea de órdenes de venta")
+
+    @api.model
+    def create(self, values_list):
+        if not values_list.get("name"):
+            raise exceptions.ValidationError("El campo de nombre de venta es obligatorio")
+        if not values_list.get("date_order"):
+            raise exceptions.ValidationError("El campo de fecha de orden de venta es obligatorio")
+        if not values_list.get("amount_total"):
+            raise exceptions.ValidationError("El campo de monto total es obligatorio")
+        if not values_list.get("order_line_ids"):
+            raise exceptions.ValidationError("La orden de venta debe tener al menos una línea de orden de venta")
+        
+        return super(Order, self).create(values_list)
+        
